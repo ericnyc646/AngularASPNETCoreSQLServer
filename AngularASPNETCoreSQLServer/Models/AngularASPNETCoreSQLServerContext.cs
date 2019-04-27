01ef94d765ae1a4285a7da31f44d75634325bba6
ericcestero <ericcestero@ERICCESTERO1BAA>
2019-04-27 15:51:16 -0400

    created data access layer

--- AngularASPNETCoreSQLServer/Models/AngularASPNETCoreSQLServerContext.cs
+++ AngularASPNETCoreSQLServer/Models/AngularASPNETCoreSQLServerContext.cs
@@ -22,7 +22,6 @@ namespace AngularASPNETCoreSQLServer.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Server=localhost;Database=AngularASPNETCoreSQLServer;Trusted_Connection=True;");
             }
         }
