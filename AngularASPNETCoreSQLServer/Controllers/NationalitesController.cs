6660b7b125bd106ccf6ffd51139d874eebd69f67
ericcestero <ericcestero@ERICCESTERO1BAA>
2019-04-27 16:37:13 -0400

    Completed WebApi
    http://localhost:56546/api/astronaut/Index
    http://localhost:56546/api/nationalities/Index

---
+++ AngularASPNETCoreSQLServer/Controllers/NationalitesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AngularASPNETCoreSQLServer.Data;
+using AngularASPNETCoreSQLServer.Models;
+
+namespace AngularASPNETCoreSQLServer.Controllers
+{
+    public class NationalitesController : ControllerBase
+    {
+
+        AstronautsDataAccessLayer astronautsDL = new AstronautsDataAccessLayer();
+
+        [HttpGet]
+        [Route("api/nationalities/Index")]
+        public IEnumerable<Nationalities> Index()
+        {
+            return astronautsDL.GetNationalities();
+        }
+    }
+}
